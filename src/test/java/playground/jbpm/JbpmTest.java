a73ccf757d306d292af5ea727d4d2d203408bffe
mark.verebelyi <nihilist84@gmail.com>
2012-06-05 17:26:39 +0200

    My first test process

---
+++ src/test/java/playground/jbpm/JbpmTest.java
@@ -0,0 +1,26 @@
+package playground.jbpm;
+
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import playground.jbpm.category.IntegrationTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring/application.xml")
+@Category(IntegrationTest.class)
+public class JbpmTest {
+
+	@Autowired
+	private StatefulKnowledgeSession ksession;
+
+	@Test
+	public void Hello_World_Process() {
+		ksession.startProcess("com.sample.bpmn");
+	}
+
+}
